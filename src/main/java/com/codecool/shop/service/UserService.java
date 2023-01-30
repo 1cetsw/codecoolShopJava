3fbc40237937da750a766388111b963e5734f402
1cetsw <1cetsw@gmail.com>
2023-01-30 11:00:54 +0100

    add supplier service and display supplier menu in home page

--- src/main/java/com/codecool/shop/service/UserService.java
+++ src/main/java/com/codecool/shop/service/UserService.java
@@ -1,29 +1,29 @@
-//package com.codecool.shop.service;
-//
-//import com.codecool.shop.dao.UserDao;
-//import com.codecool.shop.model.User;
-//import lombok.AllArgsConstructor;
-//
-//import java.util.Optional;
-//
-//@AllArgsConstructor
-//public class UserService {
-//
-//    private UserDao userDao;
-//
-//    public Optional<User> validateUser(String loginInfo, String password) {
-//
-//        Optional<User> user = userDao.getUserByLogin(loginInfo);
-//
-//        if(user.isEmpty()) {
-//            user = userDao.getUserByEmail(loginInfo);
-//            if(user.isEmpty()) {
-//                return Optional.empty();
-//            }
-//        }
-//
-//        boolean isPasswordValid = user.get().getUserPassword().equals(password);
-//        return isPasswordValid ? user : Optional.empty();
-//    }
-//
-//}
+package com.codecool.shop.service;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.User;
+import lombok.AllArgsConstructor;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserService {
+
+    private UserDao userDao;
+
+    public Optional<User> validateUser(String loginInfo, String password) {
+
+        Optional<User> user = userDao.getUserByLogin(loginInfo);
+
+        if(user.isEmpty()) {
+            user = userDao.getUserByEmail(loginInfo);
+            if(user.isEmpty()) {
+                return Optional.empty();
+            }
+        }
+
+        boolean isPasswordValid = user.get().getUserPassword().equals(password);
+        return isPasswordValid ? user : Optional.empty();
+    }
+
+}
